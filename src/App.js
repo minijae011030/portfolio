ea48220cd71ffbbf3059ba117ec88df139b10a73
Ian <105128049+minijae011030@users.noreply.github.com>
2025-03-26 13:32:11 +0900

    Fix: 프로젝트 원래대로 되돌리기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./Home/Home";
-import Result from "./Home/Result";
-import { useState } from "react";
 
 function App() {
-  const [votes, setVotes] = useState({ dog: 11, cat: 9 });
-
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/result" element={<Result votes={votes} />} />
       </Routes>
     </Router>
   );
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,75 +1,53 @@
-import { useState } from "react";
-import vote from "./service/vote.service";
-import { useNavigate } from "react-router-dom";
+import { motion, useScroll } from "framer-motion";
+import { useEffect, useState } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import Cover from "./section/cover/Cover";
+import Footer from "./section/footer/Footer";
+import Profile from "./section/profile/Profile";
+import Projects from "./section/projects/Projects";
+import Skills from "./section/skill/Skills";
 
 function Home() {
-  // 투표 수 상태
-  const [selected, setSelected] = useState(null);
-  const navigation = useNavigate();
-  // 라디오 버튼 선택
-  const handleSelect = (event) => {
-    setSelected(event.target.value);
-  };
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  // 투표 버튼 클릭
-  const handleVote = async () => {
-    if (!selected) {
-      alert("항목을 선택해주세요!");
-      return;
-    }
+  const { scrollYProgress } = useScroll();
 
-    // const res = await vote({ selected });
+  function handleResize() {
+    setIsSmallScreen(window.innerWidth <= 770);
+  }
 
-    navigation("/result");
-    return;
-  };
+  useEffect(() => {
+    AOS.init();
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <div
-      style={{
-        textAlign: "center",
-        padding: "20px",
-        maxWidth: "400px",
-        margin: "auto",
-        marginTop: "100px",
-      }}
-    >
-      <h2 style={{ fontSize: "30px", fontWeight: "1000" }}>소주🍶 vs 맥주🍺</h2>
-      <p style={{ fontSize: "20px", fontWeight: "1000" }}>어떤 주종?</p>
-
-      <div
+    <>
+      <motion.div
         style={{
-          marginTop: "100px",
+          scaleX: scrollYProgress,
+          height: 3,
+          backgroundColor: "#95FF00",
+          position: "fixed",
+          top: 0,
+          right: 0,
+          left: 0,
+          transformOrigin: "left",
+          zIndex: 1000,
         }}
-      >
-        <label style={{ fontSize: "20px" }}>
-          <input type="radio" name="vote" value={1} onChange={handleSelect} />
-          소주🍶
-        </label>
-        <label style={{ marginLeft: "15px", fontSize: "20px" }}>
-          <input type="radio" name="vote" value={2} onChange={handleSelect} />
-          맥주🍺
-        </label>
-      </div>
+      />
 
-      <button
-        onClick={handleVote}
-        style={{
-          marginTop: "15px",
-          padding: "10px 20px",
-          cursor: "pointer",
-          backgroundImage: "linear-gradient(to right, green, brown)",
-          color: "white",
-          width: "300px",
-          height: "70px",
-          borderRadius: "5px",
-          border: "none",
-          fontSize: "30px",
-        }}
-      >
-        투표하기
-      </button>
-    </div>
+      <Cover />
+      <Profile isSmallScreen={isSmallScreen} />
+      <Skills isSmallScreen={isSmallScreen} />
+      <Projects isSmallScreen={isSmallScreen} />
+      <Footer />
+    </>
   );
 }
 
